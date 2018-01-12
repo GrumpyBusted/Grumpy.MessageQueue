3eb6779571b0946fac4c432259f354cadb24ccdb
Anders Busted-Janum <abja@sdc.dk>
2018-01-12 23:42:36 +0100

    Added Solution file

---
+++ Grumpy.MessageQueue/Grumpy.MessageQueue/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grumpy.MessageQueue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grumpy.MessageQueue")]
+[assembly: AssemblyCopyright("Copyright © Anders Busted-Janum 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4e4557e1-63ca-40c0-b3fe-d3f4997158ff")]
+[assembly: AssemblyVersion("1.0.*")]
